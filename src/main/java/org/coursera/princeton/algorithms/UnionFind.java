1e3348aeb905250c3c0463a7a2c1ccc5cd95b477
Erhun Baycelik <erhun.baycelik@transferwise.com>
2020-04-03 10:52:29 +0300

    -- Add settings.gradle and UnionFind class.

---
+++ src/main/java/org/coursera/princeton/algorithms/UnionFind.java
@@ -0,0 +1,14 @@
+package org.coursera.princeton.algorithms;
+
+public class UnionFind {
+	public UnionFind(int n) {
+	}
+
+	public void union(int p, int q) {
+
+	}
+
+	public boolean hasConnection(int p, int q) {
+		return false;
+	}
+}
