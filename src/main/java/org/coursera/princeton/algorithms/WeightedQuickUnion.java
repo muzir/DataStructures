99e56ff05c4472805d924d66fcdab1494314fe65
Erhun Baycelik <erhun.baycelik@transferwise.com>
2020-04-09 18:01:35 +0300

    -- Tests are failing.

--- src/main/java/org/coursera/princeton/algorithms/WeightedQuickUnion.java
+++ src/main/java/org/coursera/princeton/algorithms/WeightedQuickUnion.java
@@ -4,23 +4,45 @@ import java.util.stream.IntStream;
 
 public class WeightedQuickUnion implements Connectivity {
 	private final int[] connections;
+	private final int[] weights;
 
 	public WeightedQuickUnion(int n) {
 		this.connections = new int[n];
-		initialize(connections);
+		this.weights = new int[n];
+		initialize();
 	}
 
-	private void initialize(int[] connections) {
+	private void initialize() {
 		IntStream.range(0, connections.length - 1).forEach(c -> connections[c] = c);
+		IntStream.range(0, weights.length - 1).forEach(c -> connections[c] = 1);
 	}
 
 	@Override
 	public void union(int p, int q) {
-
+		int pWeight = weights[p];
+		int qWeights = weights[q];
+		if (pWeight > qWeights) {
+			connections[q] = p;
+			weights[p] = weights[p] + weights[q];
+		} else {
+			connections[p] = q;
+			weights[q] = weights[q] + weights[p];
+		}
 	}
 
 	@Override
 	public boolean connected(int p, int q) {
+		if (findRoot(p) == findRoot(q)) {
+			return true;
+		}
 		return false;
+	}
+
+	private int findRoot(int valueIndex) {
+		int value = connections[valueIndex];
+		if (value == valueIndex) {
+			return value;
+		}
+		return findRoot(value);
 	}
 }
--- src/test/java/org/coursera/princeton/algorithms/WeightedQuickUnionTest.java
+++ src/test/java/org/coursera/princeton/algorithms/WeightedQuickUnionTest.java
@@ -1,16 +1,48 @@
 package org.coursera.princeton.algorithms;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class WeightedQuickUnionTest {
+class WeightedweightedQuickUnionTest {
 
 	Connectivity weightedQuickUnion = new WeightedQuickUnion(10);
 
+	private void createConnectionTree() {
+		weightedQuickUnion.union(2, 9);
+		weightedQuickUnion.union(4, 9);
+		weightedQuickUnion.union(3, 4);
+		weightedQuickUnion.union(6, 5);
+		weightedQuickUnion.union(6, 9);
+	}
+
+	@BeforeEach
+	void setup() {
+		createConnectionTree();
+	}
+
 	@Test
 	void returnTrue_ifNodesAreConnected() {
-		weightedQuickUnion.union(0, 1);
-		assertTrue(weightedQuickUnion.connected(0, 1));
+		assertTrue(weightedQuickUnion.connected(3, 6));
+		assertTrue(weightedQuickUnion.connected(3, 4));
+		assertTrue(weightedQuickUnion.connected(3, 2));
+		assertTrue(weightedQuickUnion.connected(2, 3));
+		assertTrue(weightedQuickUnion.connected(2, 6));
+		assertTrue(weightedQuickUnion.connected(2, 5));
+		assertTrue(weightedQuickUnion.connected(2, 5));
+	}
+
+	@Test
+	void returnFalse_ifNodesAreNotConnected() {
+		assertFalse(weightedQuickUnion.connected(2, 7));
+		assertFalse(weightedQuickUnion.connected(2, 8));
+		assertFalse(weightedQuickUnion.connected(0, 3));
+		assertFalse(weightedQuickUnion.connected(0, 1));
+		assertFalse(weightedQuickUnion.connected(3, 1));
+		assertFalse(weightedQuickUnion.connected(3, 0));
+		assertFalse(weightedQuickUnion.connected(3, 7));
+		assertFalse(weightedQuickUnion.connected(3, 8));
 	}
 }
