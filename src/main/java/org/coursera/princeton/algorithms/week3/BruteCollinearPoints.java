5f81477f8eba2b46a4c77ade5e77f8a7efc8bcbf
Erhun Baycelik <erhun.baycelik@transferwise.com>
2020-05-05 23:43:06 +0300

    * Week 3 assignment started;   * BruteCollinearPoints debugging.

--- src/main/java/org/coursera/princeton/algorithms/week3/BruteCollinearPoints.java
+++ src/main/java/org/coursera/princeton/algorithms/week3/BruteCollinearPoints.java
@@ -19,14 +19,14 @@ public class BruteCollinearPoints {
 		int pointLength = points.length;
 		for (int p = 0; p < pointLength; p++) {
 			Point pointP = points[p];
-			for (int q = p + 1; q < pointLength; q++) {
+			for (int q = p; q < pointLength; q++) {
 				Point pointQ = points[q];
-				for (int r = q + 1; r < pointLength; r++) {
+				for (int r = q; r < pointLength; r++) {
 					Point pointR = points[r];
-					for (int s = r + 1; s < pointLength; s++) {
+					for (int s = r; s < pointLength; s++) {
 						Point pointS = points[s];
 						if (checkCollinear(pointP, pointQ, pointR, pointS)) {
-							System.out.println("p:" + p + " q:" + q + " r:" + r + " s:" + s);
+							System.out.println("p:" + pointP.toString() + " q:" + pointQ.toString() + " r:" + pointR.toString() + " s:" + pointS.toString());
 							addLineSegments(new LineSegment(pointP, pointS));
 						}
 					}
@@ -49,6 +49,16 @@ public class BruteCollinearPoints {
 	}
 
 	private boolean checkCollinear(Point pointP, Point pointQ, Point pointR, Point pointS) {
+		if (pointP.compareTo(pointQ) == 0 ||
+				pointP.compareTo(pointQ) == 0 ||
+				pointP.compareTo(pointR) == 0 ||
+				pointP.compareTo(pointS) == 0 ||
+				pointR.compareTo(pointS) == 0 ||
+				pointQ.compareTo(pointR) == 0 ||
+				pointQ.compareTo(pointS) == 0 ||
+				pointR.compareTo(pointS) == 0) {
+			return false;
+		}
 		double pQ = pointP.slopeTo(pointQ);
 		double pR = pointP.slopeTo(pointR);
 		double pS = pointP.slopeTo(pointS);
