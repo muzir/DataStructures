6ea51b87070f1028b9e78ed33739bba607c13ab2
Erhun Baycelik <erhun.baycelik@transferwise.com>
2020-06-08 23:31:13 +0300

    * UnorderedPriorityQueue implemented.

--- src/main/java/org/coursera/princeton/algorithms/week4/UnorderedPriorityQueue.java
+++ src/main/java/org/coursera/princeton/algorithms/week4/UnorderedPriorityQueue.java
@@ -1,26 +1,38 @@
 package org.coursera.princeton.algorithms.week4;
 
-import java.util.Arrays;
-import java.util.Comparator;
-
 public class UnorderedPriorityQueue<Key extends Comparable<Key>> implements Queue<Key> {
 
 	private final Key[] container;
 	private int lastIndexPointer;
 
 	public UnorderedPriorityQueue(int capacity) {
-		this.container = (Key[]) new Object[capacity];
+		this.container = (Key[]) new Comparable[capacity];
 		this.lastIndexPointer = 0;
 	}
 
 	@Override
 	public void insert(Key key) {
+		/**TODO add container full check, compare capacity and lastIndexPointer*/
 		container[lastIndexPointer++] = key;
 	}
 
 	@Override
 	public Key delMax() {
-		return Arrays.stream(container).max(Comparator.naturalOrder()).orElse(null);
+		/**TODO add container empty check*/
+		int max = 0;
+		for (int i = 0; i < lastIndexPointer; i++) {
+			if (container[i].compareTo(container[max]) > 0) {
+				max = i;
+			}
+		}
+		swap(max, lastIndexPointer - 1);
+		return container[--lastIndexPointer];
+	}
+
+	private void swap(int max, int indexPointer) {
+		Key temp = container[max];
+		container[max] = container[indexPointer];
+		container[indexPointer] = temp;
 	}
 
 	@Override
--- src/test/java/org/coursera/princeton/algorithms/week4/UnorderedPriorityQueueTest.java
+++ src/test/java/org/coursera/princeton/algorithms/week4/UnorderedPriorityQueueTest.java
@@ -1,5 +1,28 @@
 package org.coursera.princeton.algorithms.week4;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class UnorderedPriorityQueueTest {
 
+	private UnorderedPriorityQueue underTest;
+
+	@BeforeEach
+	public void setUp() {
+		underTest = new UnorderedPriorityQueue(10);
+	}
+
+	@Test
+	void shouldDelMaxReturnMaxValue() {
+		underTest.insert(BigDecimal.ZERO);
+		underTest.insert(BigDecimal.ONE);
+		underTest.insert(BigDecimal.TEN);
+		assertEquals(BigDecimal.TEN, underTest.delMax());
+		assertEquals(BigDecimal.ONE, underTest.delMax());
+		assertEquals(BigDecimal.ZERO, underTest.delMax());
+	}
 }
