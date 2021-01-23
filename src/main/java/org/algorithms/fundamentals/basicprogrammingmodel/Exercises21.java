3f1d76d0deb0ebfdd01681c91ad1953cd6814aa0
Erhun Baycelik <erhun.baycelik@transferwise.com>
2021-01-23 21:47:09 +0200

    * 23 January Exercises.

---
+++ src/main/java/org/algorithms/fundamentals/basicprogrammingmodel/Exercises21.java
@@ -0,0 +1,20 @@
+package org.algorithms.fundamentals.basicprogrammingmodel;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class Exercises21 {
+	public static void main(String[] args) {
+		String[] lines = StdIn.readAllLines();
+		StdOut.printf("name first second result \n");
+		Arrays.stream(lines).forEach(line -> {
+			String[] parameters = line.split(" ");
+			String name = parameters[0];
+			int firstInteger = Integer.parseInt(parameters[1]);
+			int secondInteger = Integer.parseInt(parameters[2]);
+			StdOut.printf("%s %d %d %d \n", name, firstInteger, secondInteger, firstInteger / secondInteger);
+		});
+	}
+}
