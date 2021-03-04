9f6233225cc7fe582d025d500363f60f1c084458
Erhun Baycelik <erhun.baycelik@transferwise.com>
2021-03-04 23:47:36 +0200

    * Exercises 29 solved.

---
+++ src/test/java/org/algorithms/fundamentals/basicprogrammingmodel/Exercise29Test.java
@@ -0,0 +1,30 @@
+package org.algorithms.fundamentals.basicprogrammingmodel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.algorithms.fundamentals.basicprogrammingmodel.Exercise29.count;
+import static org.algorithms.fundamentals.basicprogrammingmodel.Exercise29.rank;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Exercise29Test {
+	@Test
+	void simpleDuplicateArrayTest() {
+		int[] arr = {10, 10};
+		assertEquals(0, rank(10, arr));
+		assertEquals(2, count(10, arr));
+	}
+
+	@Test
+	void midDuplicateArrayTest() {
+		int[] arr = {4, 5, 10, 10};
+		assertEquals(2, rank(10, arr));
+		assertEquals(2, count(10, arr));
+	}
+
+	@Test
+	void longDuplicateArrayTest() {
+		int[] arr = {4, 5, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+		assertEquals(2, rank(10, arr));
+		assertEquals(11, count(10, arr));
+	}
+}
