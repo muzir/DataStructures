9fec226a063119199f7d9fb5f6a2fd0d0894e233
Erhun Baycelik <erhun.baycelik@transferwise.com>
2021-03-07 21:57:10 +0200

    * Exercises 30 solved.

---
+++ src/main/java/org/algorithms/fundamentals/basicprogrammingmodel/Exercise31.java
@@ -0,0 +1,12 @@
+package org.algorithms.fundamentals.basicprogrammingmodel;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+public class Exercise31 {
+	public static void main(String[] args) {
+		StdDraw.circle(.1, .1, 0.05);
+		StdDraw.circle(.1, .2, 0.05);
+		StdDraw.setPenColor(StdDraw.GRAY);
+		StdDraw.line(.1, .1, .1, .2);
+	}
+}
