2c8847581484d36d595e8611e1b23c049ef1085a
Erhun Baycelik <erhun.baycelik@transferwise.com>
2021-03-27 13:54:00 +0200

    * Exercise33 solved.

--- src/main/java/org/algorithms/fundamentals/basicprogrammingmodel/Exercise33.java
+++ src/main/java/org/algorithms/fundamentals/basicprogrammingmodel/Exercise33.java
@@ -10,6 +10,13 @@ interface Matrix {
 	double[][] mult(double[][] a, double[][] b);
 
 	double[][] transpose(double[][] a);
+
+	double[] mult(double[][] a, double[] x);
+
+	/*TODO in the book this method dscribed as vector-vector product page 60
+	 *  but parameters are vector and matrix, not sure parameters are correct
+	 * */
+	double[] mult(double[] y, double[][] a);
 }
 
 public class Exercise33 implements Matrix {
@@ -24,6 +31,10 @@ public class Exercise33 implements Matrix {
 		printMatrix(at);
 		double[][] b = {{-1, -2, -3}, {-4, -5, -6}, {-7, -8, -9}};
 		printMatrix(matrix.mult(a, b));
+		double[][] a1 = {{1, -1, 2}, {0, -3, 1}};
+		double[] x1 = {2, 1, 0};
+		println(Arrays.toString(matrix.mult(a1, x1)));
+		println(Arrays.toString(matrix.mult(x1, a1)));
 	}
 
 	private static void printMatrix(double[][] at) {
@@ -68,5 +79,26 @@ public class Exercise33 implements Matrix {
 			}
 		}
 		return at;
+	}
+
+	@Override
+	public double[] mult(double[][] a, double[] x) {
+		if (a[0].length != x.length) {
+			throw new IllegalArgumentException("matrix column number is not equal to vector column number");
+		}
+		double[] result = new double[a.length];
+		for (int i = 0; i < a.length; i++) {
+			double acc = 0;
+			for (int j = 0; j < a[0].length; j++) {
+				acc = acc + (a[i][j] * x[j]);
+			}
+			result[i] = acc;
+		}
+		return result;
+	}
+
+	@Override
+	public double[] mult(double[] y, double[][] a) {
+		return mult(a, y);
 	}
 }
