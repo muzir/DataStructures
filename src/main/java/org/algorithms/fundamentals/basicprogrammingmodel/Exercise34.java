0d15874c66a62174bb054e369db2e6eb2a8ef6a7
Erhun Baycelik <erhun.baycelik@transferwise.com>
2021-04-06 23:08:16 +0300

    * Example 34 has not finished yet, finished 1 sub item.

--- src/main/java/org/algorithms/fundamentals/basicprogrammingmodel/Exercise34.java
+++ src/main/java/org/algorithms/fundamentals/basicprogrammingmodel/Exercise34.java
@@ -4,17 +4,35 @@ package org.algorithms.fundamentals.basicprogrammingmodel;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Exercise34 {
 	public static void main(String[] args) {
+
+		List<Double> numbers = new ArrayList<>();
+
 		/*Print the maximum and minimum numbers; filtering is enough with fixed variables*/
 		double min = 1.0;
 		double max = 0.0;
 		while (!StdIn.isEmpty()) {
-			min = Double.min(min, StdIn.readDouble());
-			max = Double.max(max, StdIn.readDouble());
+			final double parameter = StdIn.readDouble();
+			numbers.add(parameter);
+			min = Double.min(min, parameter);
+			max = Double.max(max, parameter);
 		}
-		StdOut.print("min:" + min);
-		StdOut.print("max:" + max);
 
+		StdOut.println("min:" + min + " max:" + max);
+
+		/*Print the median of the numbers*/
+		printMedianOfTheNumbers(numbers);
+	}
+
+	private static void printMedianOfTheNumbers(List<Double> numbers) {
+		int size = numbers.size();
+		Double[] sortedNumbers = numbers.stream().sorted(Comparator.naturalOrder()).toArray(Double[]::new);
+		String message = String.format("Median:%s", sortedNumbers[size / 2]);
+		StdOut.println(message);
 	}
 }
