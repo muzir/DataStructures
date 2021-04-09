e4bbf63112928e8819f9afd579238752cd09e7ca
Erhun Baycelik <erhun.baycelik@transferwise.com>
2021-04-09 23:27:43 +0300

    * ResizingArrayStack has finished.

--- src/main/java/org/algorithms/fundamentals/bagsqueuesstacks/ResizingArrayStack.java
+++ src/main/java/org/algorithms/fundamentals/bagsqueuesstacks/ResizingArrayStack.java
@@ -1,18 +1,35 @@
 package org.algorithms.fundamentals.bagsqueuesstacks;
 
-import java.util.Arrays;
 import java.util.Iterator;
+
+import static edu.princeton.cs.algs4.StdOut.println;
 
 public class ResizingArrayStack<T> implements Stack<T>, Iterable<T> {
 
-	private int n;
-	private int capacity;
-	private T[] array;
+	private int n = 0;
+	private T[] array = (T[]) new Object[1];
 
-	public ResizingArrayStack(int capacity) {
-		this.n = 0;
-		this.capacity = capacity;
-		array = (T[]) new Object[capacity];
+	public static void main(String[] args) {
+		ResizingArrayStack<String> stack = new ResizingArrayStack<>();
+		stack.push("to");
+		stack.push("be");
+		stack.push("or");
+		stack.push("not");
+		stack.push("to");
+		stack.push("be");
+
+
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.pop();
+
+
+		Iterator iterator = stack.iterator();
+		while (iterator.hasNext()) {
+			println(iterator.next());
+		}
 	}
 
 	@Override
@@ -27,21 +44,46 @@ public class ResizingArrayStack<T> implements Stack<T>, Iterable<T> {
 
 	@Override
 	public void push(T item) {
-		if (n > capacity / 2) {
-			T[] temp = Arrays.copyOf(array, array.length);
-			this.capacity = 2 * capacity;
-			array = (T[]) new Object[capacity];
+		if (array.length == n) resize(2 * array.length);
+		array[n++] = item;
+	}
 
+	private void resize(int capacity) {
+		T[] temp = (T[]) new Object[capacity];
+		for (int i = 0; i < n; i++) {
+			temp[i] = array[i];
 		}
+		array = temp;
 	}
 
 	@Override
 	public T pop() {
-		return null;
+		if (isEmpty()) {
+			throw new RuntimeException("Stack is empty");
+		}
+		T item = array[--n];
+		array[n] = null;
+		if (n > 0 && n == array.length / 4) resize(array.length / 2);
+		return item;
 	}
 
 	@Override
 	public Iterator<T> iterator() {
-		return null;
+		return new ReverseArrayIterator();
+	}
+
+	private class ReverseArrayIterator implements Iterator<T> {
+
+		private int i = n - 1;
+
+		@Override
+		public boolean hasNext() {
+			return i >= 0;
+		}
+
+		@Override
+		public T next() {
+			return array[i--];
+		}
 	}
 }
