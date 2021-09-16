3306997fba2a3141077f425d9898a7535050bf25
erhun.baycelik <erhun.baycelik@transferwise.com>
2021-09-16 15:48:49 +0300

    Add hashMap test

---
+++ src/test/java/org/HashMapTest.java
@@ -0,0 +1,22 @@
+package com.transferwise.moneytracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HashMapTest {
+    @Test
+    void foo() {
+        List<Integer> scores = List.of(6, 12, 14, 8, 12, 14, 6, 8, 12, 11, 12, 14, 8, 11, 14, 3, 6, 11, 6, 12, 14);
+        assertEquals(21, scores.size());
+        Map<Integer, List<Integer>> scoreListMap = scores.stream().collect(groupingBy(i -> i));
+        Map<Integer, Integer> scoreCounterMap = scoreListMap.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().size()));
+        System.out.println(scoreCounterMap);
+    }
+}
